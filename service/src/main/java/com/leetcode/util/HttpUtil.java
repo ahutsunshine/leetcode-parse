2890c93db1b214af3319837a40d38f9cd4e8bc88
ahutsunshine <ahutsunshine@gmail.com>
2019-02-25 14:39:33 +0800

    fix response msg null exception

--- service/src/main/java/com/leetcode/util/HttpUtil.java
+++ service/src/main/java/com/leetcode/util/HttpUtil.java
@@ -3,6 +3,7 @@ package com.leetcode.util;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.leetcode.model.response.APIResponse;
+import com.leetcode.model.response.Error;
 import com.leetcode.model.response.LeetcodeErrorMessage;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpHeaders;
@@ -18,9 +19,11 @@ import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.util.CollectionUtils;
 import org.springframework.util.StringUtils;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Map;
 
 import static com.leetcode.util.CommonUtil.decode;
@@ -146,8 +149,9 @@ public class HttpUtil {
             return JSON.parseObject(res, APIResponse.class);
         }
         if (isErrorMessage(msg)) {
-            String info = msg.getErrors().size() > 0 ?
-                    msg.getErrors().get(0).getMessage() : "Request failed";
+            List<Error> errors = msg.getErrors();
+            String info = CollectionUtils.isEmpty(errors) ?
+                    "Request failed" : msg.getErrors().get(0).getMessage();
             return new APIResponse(400, info);
         }
         return null;
